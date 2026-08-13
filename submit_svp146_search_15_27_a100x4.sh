df4f200854b3c4215b3500758ae7915f8b41c5f9
nindanaoto <matsuoka.kotaro@gmail.com>
2026-08-13 17:12:35 +0900

    Run SVP preprocessing tasks concurrently

--- submit_svp146_search_15_27_a100x4.sh
+++ submit_svp146_search_15_27_a100x4.sh
@@ -23,7 +23,7 @@ run="$(sbatch --parsable \
     --export="ALL,SOURCE_COMMIT=${SOURCE_COMMIT}" \
     run_svp146_search_a100x4.sbatch | cut -d';' -f1)"
 
-echo "prepare_array=${prep} seeds=15-27 concurrency=2"
+echo "prepare_array=${prep} seeds=15-27 concurrency=13"
 echo "run_array=${run} seeds=15-27 concurrency=2 dependency=aftercorr:${prep}"
 echo "preprocess=LLL-heuristic-mpfr256-then-PotLLLBKZ64x8 MLD=120 TSD=136 target_norm2=9142369"
 echo "logs=${LOG_ROOT}"
